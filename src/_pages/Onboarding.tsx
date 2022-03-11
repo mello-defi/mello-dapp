a67318d001b1290fa58d7c903227029448167547
Briscoooe <brianbriscoe06@gmail.com>
2022-03-11 11:40:34 +0100

    button for next step

--- src/_pages/Onboarding.tsx
+++ src/_pages/Onboarding.tsx
@@ -5,10 +5,10 @@ import { OnboardingStep } from '_redux/types/onboardingTypes';
 import { CheckCircle, ExpandLess, ExpandMore, Info } from '@mui/icons-material';
 import { DefaultTransition } from '_components/core/Transition';
 import useWalletBalance from '_hooks/useWalletBalance';
-import { stepAddGasToWallet, stepDepositAave, stepPerformSwap } from '_redux/reducers/onboardingReducer';
+import { stepAddGasToWallet, stepPerformSwap } from '_redux/reducers/onboardingReducer';
 import { setStep } from '_redux/effects/onboardingEffects';
 import { getTransactionCount } from '_services/walletService';
-import { Button } from '_components/core/Buttons';
+import { Button, ButtonSize } from '_components/core/Buttons';
 
 function OnboardingStepRow({ step }: { step: OnboardingStep }) {
   const currentStep = useSelector((state: AppState) => state.onboarding.currentStep);
@@ -117,15 +117,19 @@ export default function Onboarding() {
     }
   }, [userAddress, walletBalance]);
 
-  // useEffect(() => {
-  //   if (waitingToAdvance) {
-  //     setNextStep(currentStep);
-  //   }
-  // }, [waitingToAdvance, currentStep]);
-  //
-  // useEffect(() => {
-  //   setWaitingToAdvance(true)
-  // }, [currentStep]);
+  const advanceToNextStep = () => {
+    setWaitingToAdvance(false);
+    setNextStep(currentStep);
+  }
+  useEffect(() => {
+    if (waitingToAdvance) {
+      setNextStep(currentStep);
+    }
+  }, [waitingToAdvance, currentStep]);
+
+  useEffect(() => {
+    setWaitingToAdvance(true)
+  }, [currentStep]);
   return (
     <div>
       <div className={'px-4 mb-2 flex flex-col text-body'}>
@@ -135,24 +139,21 @@ export default function Onboarding() {
         </span>
         <Button className={'w-full my-2'} onClick={() => setOnboardingInitiated(true)}>Begin</Button>
       </div>
-      {currentStep && onboardingInitiated && (
+      {nextStep && onboardingInitiated && (
         <>
           {steps
-            .filter((step) => step.number <= currentStep?.number)
+            .filter((step) => step.number <= nextStep?.number)
             .map((step) => (
-              // <div key={step.number}>
-              //   {waitingToAdvance && currentStep?.number === step.number ? (
-              //     <div className={'flex-row-center w-full text-body-smaller'}>
-              //       <DefaultTransition isOpen={waitingToAdvance}>
-              //         <div className={'my-2'}>
-              //           Please wait while we complete the previous step.
-              //         </div>
-              //       </DefaultTransition>
-              //     </div>
-              //   ): (
+              <div key={step.number}>
+                {waitingToAdvance && nextStep?.number === step.number ? (
+                  <div className={'flex-row-center w-full justify-between text-body px-2'}>
+                    <span>Step completed!</span>
+                    <Button size={ButtonSize.SMALL} onClick={advanceToNextStep}>Next step</Button>
+                  </div>
+                ): (
                   <OnboardingStepRow key={step.number} step={step} />
-                // )}
-              // </div>
+                )}
+              </div>
             ))}
         </>
       )}
