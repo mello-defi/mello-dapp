5164024818a07d0982762b8e5c316c428e710914
Briscoooe <brianbriscoe06@gmail.com>
2022-03-22 20:42:00 +0000

    first page dashboard

--- src/_redux/reducers/uiReducer.ts
+++ src/_redux/reducers/uiReducer.ts
@@ -8,7 +8,7 @@ import {
 
 const initialState: UIState = {
   sidebarOpen: false,
-  activeTab: NavTab.FUND,
+  activeTab: NavTab.DASHBOARD,
   navLinks: [
     { tab: NavTab.DASHBOARD, title: 'Dashboard' },
     { tab: NavTab.DEPOSIT, title: 'Deposit' },
