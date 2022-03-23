f669f5b7ddc6aa5253d6da120409d145210613f4
Briscoooe <brianbriscoe06@gmail.com>
2022-03-23 19:58:36 +0000

    withdraw working

--- src/_pages/Invest/Invest.tsx
+++ src/_pages/Invest/Invest.tsx
@@ -1,7 +1,8 @@
 // import { getPoolAddress } from "@balancer-labs/balancer-js";
 // import { BigNumber, Contract, ethers } from 'ethers';
 // import { IERC20Abi } from ./IERC20.json
-import { Vault__factory } from '@balancer-labs/typechain';
+import ERC20Abi from '_enums/erc20.js';
+import { Vault__factory, WeightedPool2TokensFactory__factory } from '@balancer-labs/typechain';
 import { useSelector } from 'react-redux';
 import { AppState } from '_redux/store';
 import { Button } from '_components/core/Buttons';
@@ -12,9 +13,8 @@ import { useState } from 'react';
 import { BigNumber as AaveBigNumber } from '@aave/protocol-js';
 import { BigNumber, Contract, ethers } from 'ethers';
 import { WeightedPoolEncoder } from '@balancer-labs/balancer-js';
-import { StablePoolEncoder } from '@balancer-labs/sdk';
-import { CryptoCurrencySymbol } from '_enums/currency';
-
+import { getPoolAddress, StablePoolEncoder } from '@balancer-labs/sdk';
+import { TransactionRequest, TransactionResponse } from '@ethersproject/abstract-provider';
 //
 const defaultOptions: DefaultOptions = {
   watchQuery: {
@@ -97,8 +97,53 @@ export default function Invest () {
     console.log(pool);
   }
 
+  // const adjustAmp = (amp: OldBigNumber): OldBigNumber {
+  //   return amp.times(this.AMP_PRECISION);
+  // }
+
+
   // https://sourcegraph.com/github.com/balancer-labs/frontend-v2/-/blob/src/services/balancer/pools/weighted-pool.service.ts?L109:16
   // https://sourcegraph.com/github.com/balancer-labs/frontend-v2/-/blob/src/composables/pools/usePoolCreation.ts?L447:46#tab=def
+  const exitPool = async (pool: any) => {
+    const poolId = '0x0d34e5dd4d8f043557145598e4e2dc286b35fd4f000000000000000000000068';
+    const vaultAddress = '0xBA12222222228d8Ba445958a75a0704d566BF2C8';
+    const tokenAddresses = [
+      '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', // USDC
+      "0x2e1ad108ff1d8c782fcbbb89aad783ac49586756", // TUSD
+      "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063", // DAI
+      "0xc2132d05d31c914a87c6611c10748aeb04b58e8f"  // USDT
+    ]
+    const amountsOUt: string[] = [
+      '0',
+      '0',
+      '10000',
+      '0'
+    ];
+    // https://sourcegraph.com/github.com/balancer-labs/balancer-v2-monorepo/-/blob/pkg/pool-weighted/contracts/BaseWeightedPool.sol?L410\
+    // https://sourcegraph.com/github.com/balancer-labs/balancer-v2-monorepo/-/blob/pkg/pool-stable/contracts/StablePool.sol
+    // export const maxUint = (e: number): BigNumber => (2).pow(e).sub(1);
+    // https://sourcegraph.com/github.com/balancer-labs/balancer-v2-monorepo/-/blob/pvt/helpers/src/models/pools/weighted/WeightedPool.ts?L589
+    // const poolAddress = getPoolAddress(poolId)
+    const MaxUint256: BigNumber = BigNumber.from("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+    // console.log(MaxUint256.toString())
+    // const bptToken = new Contract(poolAddress, ERC20Abi, signer)
+    // const tx1: TransactionResponse = await bptToken.approve(userAddress, MaxUint256)
+    // await tx1.wait(3);
+
+    const vault = new Contract(vaultAddress, Vault__factory.abi, signer);
+    const tx = await vault.exitPool(
+      poolId,
+      userAddress,
+      userAddress,
+      {
+        assets: tokenAddresses,
+        minAmountsOut: amountsOUt,
+        fromInternalBalance: false,
+        userData: StablePoolEncoder.exitBPTInForExactTokensOut(amountsOUt, MaxUint256),
+      }
+    );
+    console.log(tx);
+  }
   const join = async (pool: any) => {
     try {
 
@@ -137,8 +182,20 @@ export default function Invest () {
         "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063", // DAI
         "0xc2132d05d31c914a87c6611c10748aeb04b58e8f"  // USDT
       ]
+      const poolAddress = getPoolAddress(poolId)
+      const MaxUint256: BigNumber = BigNumber.from("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+      // console.log(MaxUint256.toString())
+      // const bptToken = new Contract(poolAddress, ERC20Abi, signer)
+      // const tx1: TransactionResponse = await bptToken.approve(userAddress, MaxUint256)
+      // await tx1.wait(3);
+      const contract2 = new ethers.Contract(poolAddress, ERC20Abi, signer);
+      // const options: TransactionRequest = {};
+      // const txResponse: TransactionResponse = await contract2.approve(poolAddress, MaxUint256, options);
+      // await txResponse.wait(3);
+      const aa: BigNumber = await contract2.allowance(userAddress, contract2.address);
+      console.log(aa.toString());
       const amountsIn: string[] = [
-        '1000000',
+        '100',
         '0',
         '0',
         '0'
@@ -227,6 +284,7 @@ export default function Invest () {
             </div>
             <div>
               <Button onClick={() => join(p)}>Join</Button>
+              <Button onClick={() => exitPool(p)}>Exit</Button>
             </div>
           </div>
         )
