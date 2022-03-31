2495b9811c24620b32e35fb019fd071c4165048c
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-03-31 15:17:11 +0100

    Streamline wallet

--- src/_pages/Onboarding/StepDescriptions/StepConnectWallet.tsx
+++ src/_pages/Onboarding/StepDescriptions/StepConnectWallet.tsx
@@ -3,44 +3,14 @@ import OnboardingStepDescription from '_pages/Onboarding/OnboardingStepDescripti
 export default function StepConnectWallet() {
   const whatIsRequired = (
     <>
-      Connect your non-custodial wallet to mello
+      Connect or create a non-custodial wallet to use with mello.
       <br />
-      Don&apos;t have a wallet? No problem, follow the links to one of our how-to guides:
-      <br />
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://docs.mellodefi.com/resources/how-to-guides/wallet-setup-and-use-social-login"
-          >
-            Social Login - How to sign up
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://docs.mellodefi.com/resources/how-to-guides/wallet-setup-and-use-metamask"
-          >
-            Metamask - How to sign up
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://docs.mellodefi.com/resources/how-to-guides/wallet-use-walletconnect"
-          >
-            WalletConnect - How to sign up
-          </a>
-        </li>
-      </ul>
+      Click &apos;Connect&apos; in the top right of the screen and follow the instructions.
     </>
   );
 
   const whyIsRequired = (
-    <>A wallet is required to interact with the blockchain and access your assets</>
+    <>A wallet is required to interact with the blockchain and access your assets.</>
   );
 
   const notes = (
