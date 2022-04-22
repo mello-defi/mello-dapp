4808a436fe39e0056b16925eb346fbad2cfe11d3
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-04-22 15:00:45 +0100

    Fix og image, add error handling

--- src/_pages/Onboarding/StepDescriptions/StepConnectWallet.tsx
+++ src/_pages/Onboarding/StepDescriptions/StepConnectWallet.tsx
@@ -1,22 +1,23 @@
 import OnboardingStepDescription from '_pages/Onboarding/OnboardingStepDescription';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { AppState } from '_redux/store';
 import { checkForCouponAndRedeem } from '_services/checkForCoupon';
 
-
-
 export default function StepConnectWallet() {
-
   const userAddress = useSelector((state: AppState) => state.wallet.address);
   const isOnboardingComplete = useSelector((state: AppState) => state.onboarding.complete);
+  const [couponMessage, setCouponMessage] = useState(false);
 
   useEffect(() => {
-      if(userAddress){
-        checkForCouponAndRedeem(userAddress, isOnboardingComplete);
-      }
-  }, [userAddress])
-
+    if (userAddress) {
+      const check = async () => {
+        const isCoupounOnTheWay = await checkForCouponAndRedeem(userAddress, isOnboardingComplete);
+        setCouponMessage(isCoupounOnTheWay);
+      };
+      check();
+    }
+  }, [userAddress]);
 
   const whatIsRequired = (
     <>
