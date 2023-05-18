be65939f56532cceadb97079db263d5b9d322298
Briscoooe <brianbriscoe06@gmail.com>
2023-05-18 11:58:46 +0100

    supabase

--- src/_services/dbService.ts
+++ src/_services/dbService.ts
@@ -1,31 +1,25 @@
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
 import { Transaction, TransactionAction } from '_interfaces/db';
 import { TransactionServices } from '_enums/db';
 
-// @ts-ignore
-const supabaseUrl: string = process.env.REACT_APP_SUPABASE_URL;
-// @ts-ignore
-const supabaseAnonKey: string = process.env.REACT_APP_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export async function logTransaction(hash: string, chainId: number, service: TransactionServices, action: TransactionAction, amount?: string, symbol?: string) {
-  const transaction: Transaction = {
-    hash: hash.toLowerCase(),
-    chain_id: chainId,
-    service,
-    action,
-    amount,
-    symbol,
-  };
-  try {
-    await supabase
-      .from<Transaction>('transactions')
-      .insert([transaction])
-      .then((data) => {
-        console.log(data);
-      });
-  } catch (error) {
-    console.error(error);
-  }
+  // const transaction: Transaction = {
+  //   hash: hash.toLowerCase(),
+  //   chain_id: chainId,
+  //   service,
+  //   action,
+  //   amount,
+  //   symbol,
+  // };
+  // try {
+  //   await supabase
+  //     .from<Transaction>('transactions')
+  //     .insert([transaction])
+  //     .then((data) => {
+  //       console.log(data);
+  //     });
+  // } catch (error) {
+  //   console.error(error);
+  // }
 }
